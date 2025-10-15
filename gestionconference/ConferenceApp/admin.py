3ef93d61a52f9bbabe26d1e8a1f8683b1d7f4ecb
Montassar Chouiguir <montachouig@gmail.com>
2025-10-15 19:36:20 +0100

    Register Conference model in admin; update admin site header and index title

--- gestionconference/ConferenceApp/admin.py
+++ gestionconference/ConferenceApp/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Conference , Submission
 # Register your models here.
-admin.site.register(Conference)
+
+admin.site.header = "Conference Management Admin"
+admin.site.index_title = "Admin Dashboard"
+
 
 @admin.register(Submission)
 class SubmissionAdmin(admin.ModelAdmin):
@@ -16,3 +19,19 @@ class SubmissionAdmin(admin.ModelAdmin):
         ("logistic",{
             "fields" : ('status', 'payed', 'submission_date')
         }))
+    
+
+@admin.register(Conference)
+class ConferenceAdmin(admin.ModelAdmin):
+    list_display = ('conference_id', 'name', 'location', 'theme', 'start_date', 'end_date')
+    list_filter = ('theme', 'start_date', 'end_date')
+    search_fields = ('name', 'location', 'theme')
+    ordering = ('-start_date',)
+    fieldsets = (
+        ("Information générale", {
+            "fields": ('conference_id', 'name', 'description', 'location', 'theme', 'start_date', 'end_date')
+        }),
+        ("Dates", {
+            "fields": ('created_at', 'updated_at')
+        })
+    )
