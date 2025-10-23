c647ddf249de3034c810ff15be4e358150f323f7
Montassar Chouiguir <montachouig@gmail.com>
2025-10-23 14:11:38 +0100

    Refactor URL routing and views for conferences; add ConferenceList and ConferenceDetails views, and update templates for better structure and functionality.

--- gestionconference/ConferenceApp/urls.py
+++ gestionconference/ConferenceApp/urls.py
@@ -1,8 +1,12 @@
 
 
 from django.urls import path
-from . import views
+from .views import *
+
 
 urlpatterns = [
-   path('get_all_conferences/', views.get_all_conferences, name='get_all_conferences'),
+  # path('get_all_conferences/', views.get_all_conferences, name='get_all_conferences'),
+  path('get_all_conferences/', ConferenceList.as_view(), name='conference-list'),
+  path('conference-details/<int:conference_id>/', ConferenceDetails.as_view(), name='conference-details'),
+
 ]
--- gestionconference/ConferenceApp/views.py
+++ gestionconference/ConferenceApp/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
 from .models import Conference
+from django.views.generic import ListView , DetailView , CreateView
 
 def get_all_conferences(request):
     conferences = Conference.objects.all()
     return render(request, 'conference/conferences_list.html', {'conferences': conferences})
 
 # Create your views here.
+class ConferenceList(ListView):
+    model = Conference
+    context_object_name = 'conferences'
+    ordering = ['start_date']
+    template_name = 'conference/conferences_list.html'
+
+class ConferenceDetails(DetailView):
+    model = Conference
+    context_object_name = 'conference'
+    template_name = 'conference/conference-details.html'
+    pk_url_kwarg = 'conference_id'
+    
+class ConferenceCreate(CreateView):
+    model=Conference
+    template_name='conference/conference_form.html'
+    fields='__all__'
+    success_url=reverse_lazy('conference-list')
